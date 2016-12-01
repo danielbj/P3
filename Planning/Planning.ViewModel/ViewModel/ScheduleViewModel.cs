e25ba66cf0b44ab82053c6dd0cbcae6f83146205
Unknown <jendal@live.dk>
2016-12-01 11:47:28 +0100

    Some changes

--- Planning/Planning.ViewModel/ViewModel/ScheduleViewModel.cs
+++ Planning/Planning.ViewModel/ViewModel/ScheduleViewModel.cs
@@ -5,14 +5,57 @@ using System.Text;
 using System.Threading.Tasks;
 using Planning.Model.Employees;
 using Planning.Model.Schedules;
+using System.ComponentModel;
 
 namespace Planning.ViewModel
 {
-    class ScheduleViewModel
+    public class ScheduleViewModel : INotifyPropertyChanged
     {
-        IGroupSchedule Schedule;
-        public ScheduleViewModel(IGroupSchedule schedule) {
-            Schedule = schedule;
+        //IGroupSchedule Schedule;
+        //public ScheduleViewModel(IGroupSchedule schedule)
+        //{
+        //    Schedule = schedule;
+        //}
+
+        private GroupContainer _groupContainer = new GroupContainer();
+
+        // Internal property variables
+        private Group _selectedGroup;
+
+        // Events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ScheduleViewModel()
+        {
+            // Load from database
+            // _groupContainer.Groups = GetAllGroups();
+        }
+
+        public Group SelectedGroup
+        {
+            get
+            {
+                return _selectedGroup;
+            }
+            private set
+            {
+                _selectedGroup = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Event property invoke
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void GroupScheduleSelected()
+        {
+
         }
     }
 }
